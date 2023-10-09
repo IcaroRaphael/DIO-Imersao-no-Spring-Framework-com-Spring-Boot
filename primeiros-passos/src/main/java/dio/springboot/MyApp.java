c25e42834844dc9e0f1bbf02a674e42a266e8968
IcaroRaphael <icaro2551@gmail.com>
2023-10-09 14:38:41 -0300

    feat: Beans versus Components

--- primeiros-passos/src/main/java/dio/springboot/MyApp.java
+++ primeiros-passos/src/main/java/dio/springboot/MyApp.java
@@ -8,7 +8,7 @@ import org.springframework.stereotype.Component;
 public class MyApp implements CommandLineRunner {
     @Autowired
     private Calculadora calculadora;
-    
+
     @Override
     public void run(String... args) throws Exception {
         System.out.println("O resultado é " + calculadora.somar(2,7));
